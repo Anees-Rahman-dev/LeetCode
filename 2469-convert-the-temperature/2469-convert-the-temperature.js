ab5a95790f6cb0da9fed63bcd7a448b7dc2298eb
Anees <aneesrahman0101@gmail.com>
2026-06-06 17:50:27 +0530

    Time: 0 ms (100%), Space: 54.1 MB (15.79%) - LeetHub

--- 2469-convert-the-temperature/2469-convert-the-temperature.js
+++ 2469-convert-the-temperature/2469-convert-the-temperature.js
@@ -1,7 +1,6 @@
+
 var convertTemperature = function(celsius) {
-   
-   let kelvin = celsius +  273.15 
-   let ferrenHeat = celsius * 1.80 + 32.00
-    return [kelvin,ferrenHeat]
+
+    return  [celsius +  273.15,celsius * 1.80 + 32.00 ]
 };
 console.log(convertTemperature(122.11))
