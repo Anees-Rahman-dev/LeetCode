3eab962cfe2b4b829796dd66fdf6450cf4b5faeb
Anees <aneesrahman0101@gmail.com>
2026-06-08 19:05:48 +0530

    Time: 189 ms (5.02%), Space: 61.6 MB (5.08%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -0,0 +1,8 @@
+function isValid(s) {
+  
+    while (s.includes("()") || s.includes("[]") || s.includes("{}")) {
+        s = s.replace("()", "").replace("[]", "").replace("{}", "");
+    }
+    
+    return s === "";
+}
